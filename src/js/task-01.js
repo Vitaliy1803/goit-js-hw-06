b06607ed65727a67a012a9f1265896835b23ff75
Vitaliy1803 <35754722+Vitaliy1803@users.noreply.github.com>
2023-06-25 21:10:05 +0300

    task1

---
+++ src/js/task-01.js
@@ -0,0 +1,12 @@
+const categoriesList = document.getElementById(`categories`);
+const categoriesItems = categoriesList.children;
+
+console.log(`Number of categories:`, categoriesItems.length);
+
+Array.from(categoriesItems).forEach((item) => {
+    const categoryTitle = document.querySelector(`h2`).textContent;
+    const categoryElements = item.querySelectorAll(`li`).length;
+
+    console.log(`Category: ${categoryTitle}`);
+    console.log(`Elements: ${categoryElements}`);
+})
