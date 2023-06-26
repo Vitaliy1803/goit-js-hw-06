5ec4ac520785bc7ac71bb9bd647732271f76b71a
Vitaliy1803 <35754722+Vitaliy1803@users.noreply.github.com>
2023-06-26 14:05:16 +0300

    task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingredientsList = document.getElementById(`ingredients`);
+
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  const ingredient = ingredients[i];
+  const item = document.createElement(`li`);
+  item.textContent = ingredient;
+
+  item.classList.add(`item`);
+  ingredientsList.append(item);
+
+}
+
+
+  
