cd34fa260081c374c91904a081d0dda3068c4cae
Vitaliy1803 <35754722+Vitaliy1803@users.noreply.github.com>
2023-07-01 18:10:43 +0300

    task9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorText = document.querySelector(`.color`);
+const colorBtn = document.querySelector(`.change-color`);
+const bodyBackgroundColor = document.body;
+
+const changeBackgroundColor =  () => {
+  colorText.textContent = bodyBackgroundColor.style.backgroundColor = getRandomHexColor();
+
+};
+
+colorBtn.addEventListener(`click`, changeBackgroundColor);
