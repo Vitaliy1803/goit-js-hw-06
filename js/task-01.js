3d5bc304655128cd600bcbb4eaf927edf96cbefc
Vitaliy1803 <35754722+Vitaliy1803@users.noreply.github.com>
2023-07-03 08:18:42 +0300

    task1-var2

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,18 @@
 const categoriesList = document.getElementById(`categories`);
-const categoriesItems = categoriesList.children;
+//const categoriesItems = categoriesList.children;
+const categoriesItems = document.querySelectorAll(`.item`);
 
 console.log(`Number of categories:`, categoriesItems.length);
 
-Array.from(categoriesItems).forEach((item) => {
-    const categoryTitle = document.querySelector(`h2`).textContent;
-    const categoryElements = item.querySelectorAll(`li`).length;
-
-    console.log(`\nCategory: ${categoryTitle}`);
-    console.log(`Elements: ${categoryElements}`);
+categoriesItems.forEach((category) => {
+    console.log(category.firstElementChild.textContent);
+    console.log(category.lastElementChild.children.length);
 })
+
+// Array.from(categoriesItems).forEach((item) => {
+//     const categoryTitle = document.querySelector(`h2`).textContent;
+//     const categoryElements = item.querySelectorAll(`li`).length;
+
+//     console.log(`\nCategory: ${categoryTitle}`);
+//     console.log(`Elements: ${categoryElements}`);
+// })
