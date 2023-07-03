ab0d0214b16625a45b19978f2fa75caef3bc26e5
Vitaliy1803 <35754722+Vitaliy1803@users.noreply.github.com>
2023-07-03 10:55:59 +0300

    task9-var2

--- js/task-09.js
+++ js/task-09.js
@@ -9,8 +9,9 @@ const colorBtn = document.querySelector(`.change-color`);
 const bodyBackgroundColor = document.body;
 
 const changeBackgroundColor =  () => {
-  colorText.textContent = bodyBackgroundColor.style.backgroundColor = getRandomHexColor();
-
+  //colorText.textContent = bodyBackgroundColor.style.backgroundColor = getRandomHexColor();
+  colorText.textContent = getRandomHexColor();
+  bodyBackgroundColor.style.backgroundColor = colorText.textContent;
 };
 
 colorBtn.addEventListener(`click`, changeBackgroundColor);
