8bf388591fa0fbbe4a5b9afc5961c79922bb5d5b
Vitaliy1803 <35754722+Vitaliy1803@users.noreply.github.com>
2023-07-03 08:36:23 +0300

    task2-var2

--- js/task-02.js
+++ js/task-02.js
@@ -10,16 +10,27 @@ const ingredients = [
 
 const ingredientsList = document.getElementById(`ingredients`);
 
+// var 1
+// for (let i = 0; i < ingredients.length; i += 1) {
+//   const ingredient = ingredients[i];
+//   const item = document.createElement(`li`);
+//   item.textContent = ingredient;
 
-for (let i = 0; i < ingredients.length; i += 1) {
-  const ingredient = ingredients[i];
+//   item.classList.add(`item`);
+//   ingredientsList.append(item);
+
+// }
+
+// var1
+
+const itemRef = ingredients.map((ingredient) => {
   const item = document.createElement(`li`);
   item.textContent = ingredient;
 
-  item.classList.add(`item`);
+  item.classList.add(`item`);   
   ingredientsList.append(item);
 
-}
+});
 
 
   
