e1a30b86506f3312de991b753b0f2ab12dbc22e2
Vitaliy1803 <35754722+Vitaliy1803@users.noreply.github.com>
2023-07-03 09:28:25 +0300

    task5-var2

--- js/task-05.js
+++ js/task-05.js
@@ -1,11 +1,24 @@
 const inputStr = document.getElementById(`name-input`);
 const outputStr = document.getElementById(`name-output`);
 
-const textInput = (event) => {
-    if (inputStr.value != "") {
-        outputStr.textContent = inputStr.value;
-    }
-    
-}
 
-inputStr.addEventListener(`input`, textInput);
+// var1   (XXX) зламав
+// const textInput = (event) => {
+//     if (inputStr.value !== "") {
+//         outputStr.textContent = inputStr.value.trim();
+//     }
+//    return (outputStr.textContent = "Anonymous");
+    
+// }
+
+// inputStr.addEventListener(`input`, textInput);
+
+
+// var2
+inputStr.addEventListener(`input`, (event) => {
+    if (inputStr.value.trim() === "") {
+       return (outputStr.textContent = "Anonymous");
+    }
+    return (outputStr.textContent = inputStr.value.trim());
+
+} )
